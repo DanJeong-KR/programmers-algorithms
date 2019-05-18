073158259e2a03e1d49c45e76ad15b41e498e15a
Dodo <unhyop@gmail.com>
2019-05-18 14:02:25 +0900

    모의고사

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 | ------------- | ------------- |:-------------:|
 | [완주하지 못한 선수](https://programmers.co.kr/learn/courses/30/lessons/42576) | Counter, zip(), hash | [py](lv1/완주하지못한선수.py)|
 | [주식가격](https://programmers.co.kr/learn/courses/30/lessons/42584) | O(N^2) | [py](lv1/주식가격.py)|
+| [모의고사](https://programmers.co.kr/learn/courses/30/lessons/42840) |  | [py](lv1/모의고사.py)|
---
+++ lv1/모의고사.py
@@ -0,0 +1,24 @@
+def solution(answers):
+    answer = []
+
+    score = [0, 0, 0]
+
+    pattern = [
+        [1,2,3,4,5],
+        [2,1,2,3,2,4,2,5],
+        [3,3,1,1,2,2,4,4,5,5]
+    ]
+
+    for n, c in enumerate(answers):
+        if c == pattern[0][n%5]:
+            score[0] += 1
+        if c == pattern[1][n%8]:
+            score[1] += 1
+        if c == pattern[2][n%10]:
+            score[2] += 1
+
+    for n, c in enumerate(score):
+        if c == max(score):
+            answer.append(n+1)
+
+    return answer
